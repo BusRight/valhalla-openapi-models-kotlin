106511ce21e5ff0f59a7191e5c58d84315bedc28
gabrielsamojlo <gabrielsamojlo@gmail.com>
2026-04-23 13:40:44 +0200

    bump app version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 rootProject.name = "valhalla-models"
 
 gradle.beforeProject {
-    extensions.extraProperties["libraryVersion"] = "0.1.1"
+    extensions.extraProperties["libraryVersion"] = "0.1.1-br-patch"
 }
 
 include("client")
